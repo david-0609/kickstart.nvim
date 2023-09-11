ec4ea2653e01002618d3d3bbd6d27c0d926cd65f
David <daidavid0609@gmail.com>
2023-09-11 13:32:57 +0200

    Updated config for fidget

--- init.lua
+++ init.lua
@@ -85,7 +85,16 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+      {
+        'j-hui/fidget.nvim',
+        tag = 'legacy',
+        opts = {
+          timer = {
+            fidget_decay = 1000,
+            task_decay = 500,
+          }
+        },
+      },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
